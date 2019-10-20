b9f9f10b75227c192c3e0a0c344b7ba2759183b8
Markus Felten <Markus.Felten@gmx.de>
2019-10-20 14:58:15 +0200

    feat: add dummyResponseHandler

--- src/module.mjs
+++ src/module.mjs
@@ -2,6 +2,10 @@ export {
   interceptorTest
 } from './ava-interceptor.mjs';
 
+export {
+  dummyResponseHandler
+} from './interceptor-support.mjs';
+
 export { MockReadStream } from './mock-read-stream.mjs';
 /*
 export { MockReceiveInterceptor } from './mock-receive-interceptor.mjs';
