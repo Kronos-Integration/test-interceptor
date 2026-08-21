979656f39e832b0bfe6ae51f0a506ea36ed2f20e
Markus Felten <Markus.Felten@gmx.de>
2026-08-22 00:52:08 +0100

    test: use std import

--- tests/interceptor-ava.mjs
+++ tests/interceptor-ava.mjs
@@ -1,5 +1,5 @@
 import test from "ava";
-import { interceptorTest } from "../src/module.mjs";
+import { interceptorTest } from "@kronos-integration/test-interceptor";
 import { Interceptor } from "@kronos-integration/interceptor";
 import { Endpoint } from "@kronos-integration/endpoint";
 
